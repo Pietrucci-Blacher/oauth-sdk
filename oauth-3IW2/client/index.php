8a30578219196b63029ada9ca3c44365e73b32f9
Karl MARQUES <kmarques@vetixy.com>
2022-05-25 17:05:52 +0200

    3IW2: client done

--- oauth-3IW2/client/index.php
+++ oauth-3IW2/client/index.php
@@ -1,7 +1,9 @@
 <?php
 
 define("CLIENT_ID", '67dc2be521bec2ff862d3ab057de216b');
+define("FB_CLIENT_ID", '1311135729390173');
 define("CLIENT_SECRET", '04054cf433eeb3976252c81b6d657fda');
+define("FB_CLIENT_SECRET", 'fc5e25661fe961ab85d130779357541e');
 
 // Create a login page with a link to oauth
 function login()
@@ -23,10 +25,10 @@ function login()
     $fbQueryParams = http_build_query([
         "state"=>bin2hex(random_bytes(16)),
         "client_id"=> FB_CLIENT_ID,
-        "scope"=>"basic",
-        "redirect_uri"=>"http://localhost:8081/fb_oauth_success",
+        "scope"=>"public_profile,email",
+        "redirect_uri"=>"https://localhost/fb_oauth_success",
     ]);
-    echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a>";
+    echo "<a href=\"http://localhost:8080/auth?{$queryParams}\">Login with Oauth-Server</a><br>";
     echo "<a href=\"https://www.facebook.com/v13.0/dialog/oauth?{$fbQueryParams}\">Login with Facebook</a>";
 }
 
@@ -76,6 +78,56 @@ function callback()
     var_dump(json_decode($response, true));
 }
 
+// Facebook oauth: exchange code with token then get user info
+function fbcallback()
+{
+    $token = getToken("https://graph.facebook.com/v13.0/oauth/access_token", FB_CLIENT_ID, FB_CLIENT_SECRET);
+    $user = getFbUser($token);
+    $unifiedUser = (fn () => [
+        "id" => $user["id"],
+        "name" => $user["name"],
+        "email" => $user["email"],
+        "firstName" => $user['first_name'],
+        "lastName" => $user['last_name'],
+    ])();
+    var_dump($unifiedUser);
+}
+function getFbUser($token)
+{
+    $context = stream_context_create([
+        "http"=>[
+            "header"=>"Authorization: Bearer {$token}"
+        ]
+    ]);
+    $response = file_get_contents("https://graph.facebook.com/v13.0/me?fields=last_name,first_name,email", false, $context);
+    if (!$response) {
+        echo $http_response_header;
+        return;
+    }
+    return json_decode($response, true);
+}
+function getToken($baseUrl, $clientId, $clientSecret)
+{
+    ["code"=> $code, "state" => $state] = $_GET;
+    $queryParams = http_build_query([
+        "client_id"=> $clientId,
+        "client_secret"=> $clientSecret,
+        "redirect_uri"=>"https://localhost/fb_oauth_success",
+        "code"=> $code,
+        "grant_type"=>"authorization_code",
+    ]);
+
+    $url = $baseUrl . "?{$queryParams}";
+    $response = file_get_contents($url);
+    if (!$response) {
+        echo $http_response_header;
+        return;
+    }
+    ["access_token" => $token] = json_decode($response, true);
+
+    return $token;
+}
+
 $route = $_SERVER["REQUEST_URI"];
 switch (strtok($route, "?")) {
     case '/login':
@@ -83,6 +135,9 @@ switch (strtok($route, "?")) {
         break;
     case '/oauth_success':
         callback();
+        break;
+    case '/fb_oauth_success':
+        fbcallback();
         break;
     default:
         http_response_code(404);
